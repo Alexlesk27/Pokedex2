9739a5ca2b6057d4485299c50fd180a3567de5e5
alex silva <a0690349@gmail.com>
2022-10-03 16:21:59 -0400

    List State

---
+++ app/src/main/java/com/example/pokedex/model/ListStates.kt
@@ -0,0 +1,9 @@
+package com.example.pokedex.model
+
+sealed class ListState<out T>{
+    data class Success<out T>(val value: T) : ListState<T>()
+    data class Error(val errorId: Int = -1) : ListState<Nothing>()
+    object Empty : ListState<Nothing>()
+    object Loading : ListState<Nothing>()
+    object New : ListState<Nothing>()
+}
